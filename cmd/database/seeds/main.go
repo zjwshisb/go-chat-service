d485a15589e2b18f6526c402ab0c7b8f2becd2fa
周建威 <361340272@qq.com>
2021-07-30 22:08:27 +0800

    u

--- cmd/database/seeds/main.go
+++ cmd/database/seeds/main.go
@@ -1,12 +1,41 @@
 package main
 
 import (
+	"gorm.io/gorm"
 	"ws/app/databases"
+	"ws/app/models"
 )
 
 func init()  {
 	databases.Setup()
 }
+func rules()  {
+	rules := []models.AutoRule{
+		{
+			Name: "用户进入客服系统时",
+			MatchType: models.MatchTypeAll,
+			Match: models.MatchEnter,
+			ReplyType: models.ReplyTypeMessage,
+			IsSystem: 1,
+		},
+		{
+			Name: "当转接到人工客服而没有客服在线时(如不设置则继续转接到人工客服)",
+			MatchType: models.MatchTypeAll,
+			Match: models.MatchServiceAllOffLine,
+			ReplyType: models.ReplyTypeMessage,
+			IsSystem: 1,
+		},
+	}
+	for _, rule := range rules {
+		query := databases.Db.
+			Where("`match`=?" , rule.Match).
+			First(&models.AutoRule{})
+		if query.Error == gorm.ErrRecordNotFound {
+			databases.Db.Save(&rule)
+		}
+	}
+}
+
 func main()  {
 	rules()
 }
